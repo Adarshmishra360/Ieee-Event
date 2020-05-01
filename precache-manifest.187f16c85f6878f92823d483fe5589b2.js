8af5352aa2d8ec2f736b679f030b473769388784
Adarsh <techwizardadarsh@gmail.com>
2020-05-01 22:32:06 +0530

    Updates

---
+++ precache-manifest.187f16c85f6878f92823d483fe5589b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb65107ae36f449d647924cbb3d2471b",
+    "url": "/Ieee-Event/index.html"
+  },
+  {
+    "revision": "6e27acf7c9de8fa14e2e",
+    "url": "/Ieee-Event/static/css/main.ad776318.chunk.css"
+  },
+  {
+    "revision": "423574807d625e9ad3d1",
+    "url": "/Ieee-Event/static/js/2.de2fef5d.chunk.js"
+  },
+  {
+    "revision": "01db29174a5d10e665a9e62af351bae5",
+    "url": "/Ieee-Event/static/js/2.de2fef5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e27acf7c9de8fa14e2e",
+    "url": "/Ieee-Event/static/js/main.8d08469e.chunk.js"
+  },
+  {
+    "revision": "7f4ab2e698105e7f5c6e",
+    "url": "/Ieee-Event/static/js/runtime-main.2e274676.js"
+  },
+  {
+    "revision": "937f83f0de913a30b0e24f3ccea3df7f",
+    "url": "/Ieee-Event/static/media/slide_one.937f83f0.jpeg"
+  },
+  {
+    "revision": "3c6eb1cd4692658e5c94f705dff26c6c",
+    "url": "/Ieee-Event/static/media/slide_three.3c6eb1cd.jpeg"
+  },
+  {
+    "revision": "bd37555003ed82eed3415b55c00b894a",
+    "url": "/Ieee-Event/static/media/slide_two.bd375550.jpg"
+  }
+]);
